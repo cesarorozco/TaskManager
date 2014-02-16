0afd11147a14c57ea74cb5d71f474ed085a273ac
root <c.orozcomarin@gmail.com>
2014-02-16 16:05:21 -0500

    Validation user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :lastname, :name
+  validates :email, presence: true
+  validates :lastname, presence: true
+  validates :name, presence: true
 end
